f0623cad65cb09f462337b31100aae7ca4a9a917
Harshit Raheja <quantumbytestudios@gmail.com>
2023-06-21 01:40:40 +0530

    Update: New Look

---
+++ js/components.js
@@ -0,0 +1,96 @@
+let footer = `
+  <footer class="bg-black border-top border-dark">
+    <div class="container py-vh-4 text-secondary fw-lighter">
+      <div class="row">
+        <div class="col-12 col-lg-5 py-4 text-center text-lg-start">
+          <a class="navbar-brand pe-md-4 fs-4 col-12 col-md-auto text-center" href=".">
+            <img width="48" class="img-fluid" src="img/tube.png" alt="Overseas Logo">
+            <span class="ms-md-1 mt-1 fw-bolder me-md-5">Overseas</span>
+          </a>
+
+        </div>
+        <div class="col border-end border-dark">
+          <span class="h6">Services</span>
+          <ul class="nav flex-column">
+            <li class="nav-item">
+              <span class="link-fancy link-fancy-light cursor-pointer">Graphic Designing</span>
+            </li>
+            <li class="nav-item">
+              <span class="link-fancy link-fancy-light cursor-pointer">Video Editing</span>
+            </li>
+            <li class="nav-item">
+              <span class="link-fancy link-fancy-light cursor-pointer">Brand Design</span>
+            </li>
+            <li class="nav-item">
+              <span class="link-fancy link-fancy-light cursor-pointer">Content Creation</span>
+            </li>
+            <li class="nav-item">
+              <span class="link-fancy link-fancy-light cursor-pointer">Web Development</span>
+            </li>
+            <li class="nav-item">
+              <span class="link-fancy link-fancy-light cursor-pointer">UX/UI</span>
+            </li>
+            <li class="nav-item">
+              <span class="link-fancy link-fancy-light cursor-pointer">SEO</span>
+            </li>
+            <li class="nav-item">
+              <span class="link-fancy link-fancy-light cursor-pointer">Social Media Management</span>
+            </li>
+            <li class="nav-item">
+              <span class="link-fancy link-fancy-light cursor-pointer">Advertising</span>
+            </li>
+          </ul>
+        </div>
+        <div class="col">
+          <span class="h6">Support</span>
+          <ul class="nav flex-column">
+            <li class="nav-item">
+              <a href="about" class="link-fancy link-fancy-light">About us</a>
+            </li>
+            <li class="nav-item">
+              <a href="contact" class="link-fancy link-fancy-light">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="container text-center small py-vh-2 border-top border-dark">Made by
+      <a href="https://quantumbyte.studio/" class="link-fancy link-fancy-light" target="_blank">QuantumByteStudios</a>
+      © Copyright Overseas Marketing
+      All Rights Reserved</a>
+    </div>
+  </footer>
+`;
+
+let navbar = `
+  <nav id="navScroll" class="navbar navbar-dark bg-black fixed-top px-vw-5" tabindex="0">
+    <div class="container-fluid">
+      <a class="navbar-brand pe-md-4 fs-4 col-12 col-md-auto text-center" href=".">
+        <img width="48" class="img-fluid navLogo" src="img/tube.png" alt="Overseas Logo">
+        <span class="ms-md-1 mt-1 fw-bolder me-md-5">Overseas</span>
+      </a>
+      <div class="navbar-nav list-group list-group-horizontal">
+        <li class="nav-item">
+          <a class="nav-link fs-5" href="." aria-label="Homepage">
+            Home
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link fs-5" href="services" aria-label="A services page">
+            Services
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link fs-5" href="about" aria-label="An about page">
+            About
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link fs-5" href="contact" aria-label="A contact page">
+            Contact
+          </a>
+        </li>
+      </div>
+    </div>
+  </nav>
+`;
