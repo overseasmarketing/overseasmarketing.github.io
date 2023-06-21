5403af0e023527e5f39bb8203fd36236ab46058a
Harshit Raheja <quantumbytestudios@gmail.com>
2023-06-22 04:17:58 +0530

    Update

--- js/components.js
+++ js/components.js
@@ -22,23 +22,32 @@ let footer = `
             <li class="nav-item">
               <span class="link-fancy link-fancy-light cursor-pointer">Brand Design</span>
             </li>
-            <li class="nav-item">
+            <!--<li class="nav-item">
               <span class="link-fancy link-fancy-light cursor-pointer">Content Creation</span>
-            </li>
+            </li>-->
             <li class="nav-item">
               <span class="link-fancy link-fancy-light cursor-pointer">Web Development</span>
             </li>
-            <li class="nav-item">
+            <!--<li class="nav-item">
               <span class="link-fancy link-fancy-light cursor-pointer">UX/UI</span>
-            </li>
+            </li>-->
             <li class="nav-item">
               <span class="link-fancy link-fancy-light cursor-pointer">SEO</span>
             </li>
-            <li class="nav-item">
+            <!--<li class="nav-item">
               <span class="link-fancy link-fancy-light cursor-pointer">Social Media Management</span>
             </li>
             <li class="nav-item">
               <span class="link-fancy link-fancy-light cursor-pointer">Advertising</span>
+            </li>-->
+            <br>
+            <li class="nav-item">
+              <span class="cursor-pointer">
+                <a href="services" class="link-fancy link-fancy-light me-2">Tell me more</a>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
+                </svg>
+              </span>
             </li>
           </ul>
         </div>
