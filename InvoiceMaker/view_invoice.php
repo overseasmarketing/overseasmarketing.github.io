7b84f21b94b9e0d90ce0fb1df3a00679cb945af3
QuantumByteStudios <quantumbytestudios@gmail.com>
2024-08-22 00:28:30 +0530

    Update

--- InvoiceMaker/view_invoice.php
+++ InvoiceMaker/view_invoice.php
@@ -11,30 +11,30 @@
     <!-- Animations -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
     <style>
-        body {
-            font-family: 'Arial', sans-serif;
-        }
+    body {
+        font-family: 'Arial', sans-serif;
+    }
 
-        .text-start {
-            text-align: left;
-        }
+    .text-start {
+        text-align: left;
+    }
 
-        .text-end {
-            text-align: right;
-        }
+    .text-end {
+        text-align: right;
+    }
 
-        .text-center {
-            text-align: center;
-        }
+    .text-center {
+        text-align: center;
+    }
 
-        .opacity-75 {
-            opacity: 0.75;
-        }
+    .opacity-75 {
+        opacity: 0.75;
+    }
 
-        .invoice-table th,
-        .invoice-table td {
-            text-align: center;
-        }
+    .invoice-table th,
+    .invoice-table td {
+        text-align: center;
+    }
     </style>
 </head>
 
@@ -124,6 +124,7 @@
 
     } else if ($InvoiceAuthor == "Ishwar Chawla") {
         $authorizedSignature = "Ishwar Chawla";
+        $authorizedSignatureImage = "signature_ishwar.png";
     }
 
     $sql_clients = "SELECT * FROM clients WHERE ClientID = $clientID";
